a01d50d0f6e370f5117d5ae858ff2fed706aabd6
Alexander <protonull@protonmail.com>
2023-05-16 21:40:34 +0100

    Setup Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    id("java-library")
+    id("net.civmc.civgradle") version "2.+" apply false
+    // https://github.com/PaperMC/paperweight/tags
+    id("io.papermc.paperweight.userdev") version "1.5.5"
+    // https://github.com/johnrengelman/shadow/releases
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+dependencies {
+    paperweight.paperDevBundle(rootProject.libs.versions.minecraft)
+}
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "net.civmc.civgradle")
+    apply(plugin = "io.papermc.paperweight.userdev")
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+
+    tasks {
+        build {
+            dependsOn(shadowJar)
+            dependsOn(reobfJar)
+        }
+        test {
+            useJUnitPlatform()
+            testLogging {
+                events(
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+                )
+                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+                showCauses = true
+                showExceptions = true
+                showStackTraces = true
+            }
+        }
+        shadowJar {
+            mergeServiceFiles()
+            archiveClassifier.set("") // Prevent the -all suffix
+        }
+    }
+
+    dependencies {
+        paperweight.paperDevBundle(rootProject.libs.versions.minecraft)
+
+        compileOnly(rootProject.libs.lombok)
+        annotationProcessor(rootProject.libs.lombok)
+        testCompileOnly(rootProject.libs.lombok)
+        testAnnotationProcessor(rootProject.libs.lombok)
+
+        testImplementation(rootProject.libs.junitApi)
+        testRuntimeOnly(rootProject.libs.junitEngine)
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://repo.civmc.net/repository/maven-public")
+    }
+}
