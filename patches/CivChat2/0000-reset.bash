b8a34c31416c8aafdf0f907726da94b53f502aac
Alexander <protonull@protonmail.com>
2023-05-19 21:45:54 +0100

    Add CivChat2 submodule and patches

---
+++ patches/CivChat2/0000-reset.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd "$1" || exit 1
+
+# Checkout the submodule upstream commit
+git checkout --quiet -B upstream "6d3c5f2952cdd2f8b4a11d7a278b07f5f0e5ae6c"
+
+# Relocate Java src to root of submodule
+git mv "paper/src" "src"
+git rm -rfq "paper"
+
+# Remove submodule's Gradle wrapper and files
+git rm -rfq "gradle" "gradlew" "gradle.properties" "settings.gradle.kts"
+
+# Remove .editorconfig
+git rm -rfq ".editorconfig"
+
+# Remove Github workflows
+git rm -rfq  ".github"
+
+git commit --quiet -m "Reset CivChat2"
+git checkout --quiet -B master upstream
