78b2e0710627ea8a73d3117c7df45866c138b6fc
Alexander <protonull@protonmail.com>
2023-06-03 06:59:00 +0100

    Fix undo changes script

--- scripts/undo-changes.sh
+++ scripts/undo-changes.sh
@@ -17,9 +17,22 @@
 filesToReset=("${@:1}")
 for fileToReset in "${filesToReset[@]}"; do
     echo "Resetting $fileToReset"
+
+    # Get the current content of the file.
     currentFileContent=$(git show "HEAD:$fileToReset")
+
+    # Amend the comment to not have changed the file.
     git restore --quiet --source=HEAD~1 "$fileToReset"
     git add "$fileToReset"
     git commit --quiet --amend --no-edit --allow-empty
+
+    # Ensure the folder is still there just in case the file was the only one
+    # remaining in that folder and the folder was deleted.
+    currentFileFolder=$(dirname "$fileToReset")
+    if ! [ -d "$currentFileFolder" ]; then
+        mkdir -p "$currentFileFolder"
+    fi
+
+    # Restore the content of the file.
     echo "$currentFileContent" > "$fileToReset"
 done
