4d146c9c165137d4dc2afb55ceb3af61d35e99f3
Alexander <protonull@protonmail.com>
2023-06-27 06:55:32 +0100

    Add Citadel submodule and patches

---
+++ patches/Citadel/0000-reset.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$1" || exit 1
+
+git mv "paper/src" "src"
+git rm -rfq "paper"
+git commit --quiet -m "Flatten project structure"
+
+git rm -rfq "gradle" "gradlew" "gradlew.bat"
+git commit --quiet -m "Remove Gradle wrapper"
+
+git rm -rfq ".github" ".editorconfig" "build.gradle.kts" "gradle.properties" "settings.gradle.kts"
+git commit --quiet -m "Remove extraneous boilerplate"
+
+git checkout --quiet -B patches ignored
