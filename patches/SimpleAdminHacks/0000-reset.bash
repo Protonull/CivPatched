d61a9cfbc9f0a8dc09fbe5342081906094474bdd
Alexander <protonull@protonmail.com>
2023-07-03 22:27:18 +0100

    Updates to CMC, CC2, IE, and SAH
    CivModCore:
    - Fixed compile error from NBTCompound still using now-deleted NBTTagListExtensions methods.
    - Removed NBTTests that test now-deleted NBTHelper methods.
    - Removed ACivMod.getInstance()
    - Improves NBTCompound's switchKey() and setUUID().
    - Added new NBTList wrapper class.
    - Adds getList() and setList() to NBTCompound.
    - Adds asNBT() to PersistentDataContainerExtensions.
    - Deletes NBTSerialization's fromMap() and fromList() and deprecates fromPDC().
    
    CivChat2:
    - Newfriends are automatically entered into global chat upon first login.
    
    ItemExchange:
    - Modifiers no longer have Integer.MAX_VALUE as their default order.
    
    SimpleAdminHacks:
    - Deleted test config file. It's entirely unused.

--- patches/SimpleAdminHacks/0000-reset.bash
+++ patches/SimpleAdminHacks/0000-reset.bash
@@ -8,12 +8,10 @@ git commit --quiet -m "Flatten project structure"
 git rm -rfq "gradle" "gradlew" "gradlew.bat"
 git commit --quiet -m "Remove Gradle wrapper"
 
-git rm -rfq ".github" ".editorconfig" "build.gradle.kts" "gradle.properties" "settings.gradle.kts"
-git commit --quiet -m "Remove extraneous boilerplate"
-
-# Remove old classes
-git rm -rfq \
+git rm -rfq ".github" ".editorconfig" \
+    "build.gradle.kts" "gradle.properties" "settings.gradle.kts" \
+    "src/test/resources/config.yml" \
     "src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/EventDebugHack.java"
-git commit --quiet -m "Remove extraneous code"
+git commit --quiet -m "Remove extraneous files"
 
 git checkout --quiet -B patches ignored
